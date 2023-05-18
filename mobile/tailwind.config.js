a9d243573be78402f35f7b20e9358aa12dd44ce3
uDaanilo <daniloskype459@gmail.com>
2023-05-18 01:06:02 -0300

    aula 1

---
+++ mobile/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{jx,jsx,ts,tsx}", "./app/**/*.{jx,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
